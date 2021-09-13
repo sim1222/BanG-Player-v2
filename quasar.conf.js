28515faae60c2896e1f6b0e2f26fbbebe491057e
こけっち <50144466+sim1222@users.noreply.github.com>
2021-09-13 23:21:10 +0900

    Update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -7,7 +7,7 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 
-module.exports = function (ctx) {
+module.exports = function (/* ctx */) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
@@ -51,7 +51,8 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
-      distDir: ctx.mode.spa ? 'public' : null,
+      
+      // distDir: ctx.mode.spa ? 'public' : null,
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
